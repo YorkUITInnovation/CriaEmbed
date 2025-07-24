12bd270cd68edf8ea677fab2126be657d13f580e
isaackogan <isaacikogan@gmail.com>
2025-07-24 12:12:22 +0200

    Fix build issues

--- api/src/models/LimitGenerator.ts
+++ api/src/models/LimitGenerator.ts
@@ -1,7 +1,7 @@
 import rateLimit, {
-    RateLimitExceededEventHandler,
-    RateLimitRequestHandler,
-    ValueDeterminingMiddleware
+  RateLimitExceededEventHandler,
+  RateLimitRequestHandler,
+  ValueDeterminingMiddleware
 } from "express-rate-limit";
 import {CriaResponse} from "./CriaResponse";
 import {Options} from "express-rate-limit/dist";
@@ -11,19 +11,19 @@ import {Config} from "../config";
 
 function generateHandler(message?: string): RateLimitExceededEventHandler {
 
-    function handler(_: Request, res: Response): void {
+  function handler(_: Request, res: Response): void {
 
-        const payload: CriaResponse = {
-            timestamp: Date.now().toString(),
-            status: 429,
-            code: "TOO_MANY_REQUESTS",
-            message: message || "You hit the rate limit for this (to prevent accidents or abuse costing $20,000 in a day)"
-        }
-
-        res.status(429).json(payload);
+    const payload: CriaResponse = {
+      timestamp: Date.now().toString(),
+      status: 429,
+      code: "TOO_MANY_REQUESTS",
+      message: message || "You hit the rate limit for this (to prevent accidents or abuse costing $20,000 in a day)"
     }
 
-    return handler;
+    res.status(429).json(payload);
+  }
+
+  return handler;
 
 }
 
@@ -32,31 +32,31 @@ export default function LimitGenerator(
     options: Partial<Options>,
     keyGenerators: Array<ValueDeterminingMiddleware<string>>
 ): RateLimitRequestHandler[] {
-    const limiters: RateLimitRequestHandler[] = [];
+  const limiters: RateLimitRequestHandler[] = [];
 
-    for (const generator of keyGenerators) {
-        options["keyGenerator"] = generator;
-        options["handler"] = generateHandler(options.message);
+  for (const generator of keyGenerators) {
+    options["keyGenerator"] = generator;
+    options["handler"] = generateHandler(options.message);
 
-        limiters.push(
-            rateLimit(options)
-        );
+    limiters.push(
+        rateLimit(options)
+    );
 
-    }
+  }
 
-    return limiters;
+  return limiters;
 
 }
 
 
 function getIP(req: Request): string {
-    return req.headers['x-forwarded-for']?.toString() || req.ip;
+  return req.headers['x-forwarded-for']?.toString() || req.ip as string;
 }
 
 
 const KEY_GENERATORS: ValueDeterminingMiddleware<string>[] = [
-    async (req: Request, _: Response): Promise<string> => getIP(req),
-    async (req: Request, _: Response): Promise<string> => req.headers['Host']?.toString() || getIP(req)
+  async (req: Request, _: Response): Promise<string> => getIP(req),
+  async (req: Request, _: Response): Promise<string> => req.headers['Host']?.toString() || getIP(req)
 ]
 
 
@@ -85,9 +85,9 @@ const RATE_LIMIT_EMBED_DAY_HANDLERS = LimitGenerator(
 )
 
 export const RATE_LIMIT_CHAT_ALL_HANDLERS = [
-    ...RATE_LIMIT_CHAT_MINUTE_HANDLERS, ...RATE_LIMIT_CHAT_HOUR_HANDLERS, ...RATE_LIMIT_CHAT_DAY_HANDLERS
+  ...RATE_LIMIT_CHAT_MINUTE_HANDLERS, ...RATE_LIMIT_CHAT_HOUR_HANDLERS, ...RATE_LIMIT_CHAT_DAY_HANDLERS
 ]
 
 export const RATE_LIMIT_EMBED_ALL_HANDLERS = [
-    ...RATE_LIMIT_EMBED_MINUTE_HANDLERS, ...RATE_LIMIT_EMBED_HOUR_HANDLERS, ...RATE_LIMIT_EMBED_DAY_HANDLERS
+  ...RATE_LIMIT_EMBED_MINUTE_HANDLERS, ...RATE_LIMIT_EMBED_HOUR_HANDLERS, ...RATE_LIMIT_EMBED_DAY_HANDLERS
 ]
