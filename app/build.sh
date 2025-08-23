34c30b1a25db44cfbc5fec23a407eec5af2406ef
Patrick Thibaudeau <thibaud@yorku.ca>
2025-08-23 08:22:28 -0400

    Updated build.sh

--- app/build.sh
+++ app/build.sh
@@ -9,4 +9,4 @@ sleep 2
 docker buildx build --push \
 --platform linux/amd64,linux/arm64 \
 --tag uitadmin/criaembed-app:latest-nomic \
---tag uitadmin/criaembed-app:v0.5.2-nomic.
+--tag uitadmin/criaembed-app:v0.5.2-nomic .
